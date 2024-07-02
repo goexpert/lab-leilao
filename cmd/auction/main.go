3bdb9bf1fa6b25868b99764ac0921c4eddd370ac
Rogerio Tadim <rogeriotadim@gmail.com>
2024-07-02 02:53:47 +0000

    wip: until repo bids

--- cmd/auction/main.go
+++ cmd/auction/main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
 	"log"
 
+	"github.com/goexpert/lab-leilao/configuration/database/mongodb"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	ctx := context.Background()
+
 	if err := godotenv.Load("cmd/auction/.env"); err != nil {
 		log.Fatal("Error loading variables")
+		return
+	}
+
+	_, err := mongodb.NewMongoDBConnection(ctx)
+	if err != nil {
+		log.Fatal(err.Error())
 		return
 	}
 }
